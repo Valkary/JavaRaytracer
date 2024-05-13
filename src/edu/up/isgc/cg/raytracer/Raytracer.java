4ecb8c66e3605ba53a8f54812949996e796c6307
José Salcedo Uribe <pepoclesng@gmail.com>
2024-05-13 14:28:33 -0600

    reflections

--- src/edu/up/isgc/cg/raytracer/Raytracer.java
+++ src/edu/up/isgc/cg/raytracer/Raytracer.java
@@ -31,21 +31,28 @@ public class Raytracer {
         scene02.addObject(new Sphere(new Vector3D(0.0, 1.0, 5.0), 0.5, Color.RED));
         scene02.addObject(new Sphere(new Vector3D(0.5, 1.0, 4.5), 0.25, new Color(200, 255, 0, 1)));
         scene02.addObject(new Sphere(new Vector3D(0.35, 1.0, 4.5), 0.3, Color.BLUE));
-        scene02.addObject(new Sphere(new Vector3D(4.85, 1.0, 4.5), 0.3, Color.PINK));
+
+        Sphere pinkSphere = new Sphere(new Vector3D(4.85, 1.0, 4.5), 0.3, Color.PINK);
+        pinkSphere.setReflectivity(0.5);
+
+        scene02.addObject(pinkSphere);
         scene02.addObject(new Sphere(new Vector3D(2.85, 1.0, 304.5), 0.5, Color.BLUE));
         scene02.addObject(OBJReader.getModel3D("CubeQuad.obj", new Vector3D(-3.0, -2.5, 3.0), Color.GREEN));
 
         Model3D teapot = OBJReader.getModel3D("SmallTeapot.obj", new Vector3D(2.0, -1.0, 1.5), Color.BLUE);
         Model3D cube = OBJReader.getModel3D("Cube.obj", new Vector3D(0f, -2.5, 10.0), Color.WHITE);
+        Model3D plane = OBJReader.getModel3D("Plane.obj", new Vector3D(0.0, -2.0, 0), Color.WHITE);
 
-        teapot.setScale(0.7);
-        teapot.setRotation(rotation);
-        teapot.setDiffuse(10);
-        teapot.setReflectivity(0.8);
+//        teapot.setScale(0.7);
+//        teapot.setRotation(rotation);
+//        teapot.setDiffuse(10);
+        teapot.setReflectivity(0.75);
+        plane.setReflectivity(1);
 
         scene02.addObject(teapot);
         scene02.addObject(cube);
         scene02.addObject(OBJReader.getModel3D("Ring.obj", new Vector3D(2.0, -1.0, 1.5), Color.BLUE));
+        scene02.addObject(plane);
 
         BufferedImage image = parallelImageRaytracing(scene02);
 //        BufferedImage image = imageRaytracing(scene02);
@@ -232,46 +239,44 @@ public class Raytracer {
 
     public static Color trace(Ray ray, List<Object3D> objects, Object3D caster, double[] clippingPlanes, double depth) {
         if (depth >= MAX_RAY_DEPTH) {
-            return null;
+            return Color.BLACK;
         }
 
         Intersection closestIntersection = null;
-        Color color = Color.BLACK;
 
-        for (int i = 0; i < objects.size(); i++) {
-            Object3D currObj = objects.get(i);
+        for (Object3D currObj : objects) {
             if (!currObj.equals(caster)) {
                 Intersection intersection = currObj.getIntersection(ray);
-                if (intersection != null) {
-                    double distance = intersection.getDistance();
-                    double intersectionZ = intersection.getPosition().getZ();
-
-                    if (distance >= 0 &&
-                            (closestIntersection == null || distance < closestIntersection.getDistance()) &&
-                            (clippingPlanes == null || (intersectionZ >= clippingPlanes[0] && intersectionZ <= clippingPlanes[1]))) {
-
-                        closestIntersection = intersection;
-                        color = closestIntersection.getObject().getColor();
-
-                        if (intersection.getObject().getReflectivity() > 0) {
-                            Vector3D reflectedDirection = reflect(ray.getDirection(), intersection.getNormal());
-                            Ray reflectedRay = new Ray(intersection.getPosition(), reflectedDirection);
-                            Color hitColor = trace(reflectedRay, objects, caster, clippingPlanes, depth + 1);
-
-                            if (hitColor != null) {
-                                color = new Color(
-                                        (int)(closestIntersection.getObject().getReflectivity() * hitColor.getRed()),
-                                        (int)(closestIntersection.getObject().getReflectivity() * hitColor.getGreen()),
-                                        (int)(closestIntersection.getObject().getReflectivity() * hitColor.getBlue())
-                                );
-                            }
+                if (intersection != null && intersection.getDistance() >= 0) {
+                    if (closestIntersection == null || intersection.getDistance() < closestIntersection.getDistance()) {
+                        if (clippingPlanes == null || (intersection.getPosition().getZ() >= clippingPlanes[0] && intersection.getPosition().getZ() <= clippingPlanes[1])) {
+                            closestIntersection = intersection;
                         }
                     }
                 }
             }
         }
 
-        return color;
+        if (closestIntersection == null) {
+            return Color.BLACK;
+        }
+
+        Color objColor = closestIntersection.getObject().getColor();
+        if (closestIntersection.getObject().getReflectivity() > 0 && depth + 1 < MAX_RAY_DEPTH) {
+            Vector3D reflectedDirection = reflect(ray.getDirection(), closestIntersection.getNormal());
+            Ray reflectedRay = new Ray(closestIntersection.getPosition(), reflectedDirection);
+            Color reflectedColor = trace(reflectedRay, objects, closestIntersection.getObject(), clippingPlanes, depth + 1);
+            return blendColors(objColor, reflectedColor, closestIntersection.getObject().getReflectivity());
+        } else {
+            return objColor;
+        }
+    }
+
+    public static Color blendColors(Color baseColor, Color reflectedColor, double reflectivity) {
+        int r = (int) (baseColor.getRed() * (1 - reflectivity) + reflectedColor.getRed() * reflectivity);
+        int g = (int) (baseColor.getGreen() * (1 - reflectivity) + reflectedColor.getGreen() * reflectivity);
+        int b = (int) (baseColor.getBlue() * (1 - reflectivity) + reflectedColor.getBlue() * reflectivity);
+        return new Color(Math.min(r, 255), Math.min(g, 255), Math.min(b, 255));
     }
 
     public static Vector3D reflect(Vector3D I, Vector3D N) {
